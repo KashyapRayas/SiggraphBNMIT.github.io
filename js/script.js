c3650e0dacb6f859de9f0e8bc9f8cc1916c650c1
KashyapRayas <kashyap.rayas@gmail.com>
2022-12-04 19:44:43 +0530

    //

--- js/script.js
+++ js/script.js
@@ -1,2 +1,26 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+var checkbox = document.querySelector( '#menu-btn' );
+var icon = document.querySelector( '.menu-icon .navicon' );
+        
+var listener = function( e ) {
+    if( e.target != checkbox && e.target != icon ) {
+    checkbox.checked = false;
+    document.removeEventListener( 'click', listener );
+    }
+};
+
+checkbox.addEventListener( 'click', function(){
+    if( this.checked ) {
+    document.addEventListener( 'click', listener );
+    } 
+});
 
